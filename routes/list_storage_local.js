8f846679ea45d6d84e4a6b8fc930860482100a11
Rachel Clark <rachel.mae.clark@gmail.com>
2016-01-08 16:35:00 -0500

    Create standard sublists when creating the lists

--- routes/list_storage_local.js
+++ routes/list_storage_local.js
@@ -11,10 +11,24 @@ module.exports = {
   },
 
   add_list: function(list_name, callback) {
+    add_sublist = this.add_sublist;
     var store = storage(root + '/' + list_name);
     store.make_root(function() {
-      callback(null, list_name);
+      add_sublist(list_name, 'todo', function (err, data) {
+        add_sublist(list_name, 'keep', function (err, data) {
+          add_sublist(list_name, 'toss', function (err, data) {
+            callback(null, list_name);
+          });
+        });
+      });
     });
+  },
+
+  add_sublist: function(list_name, sublist_name, callback) {
+    var store = storage(root + '/' + list_name + '/' + sublist_name);
+    store.make_root(function() {
+      callback(null, root + '/' + list_name + '/' + sublist_name);
+    })
   },
 
   remove_list: function(list_name, callback) {
